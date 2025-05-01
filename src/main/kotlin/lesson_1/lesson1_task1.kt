d14279becb70e76348e17bdbb9a948f9d2eaa2bd
Alest-58 <emalst@mail.ru>
2025-05-01 17:06:02 +0300

    KS-1-1 create smth

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main () {
+    //val shopOfOrders: Int = 75
+    //val thanksPurchase: String = "Спасибо за покупку!"
+    val shopOfOrders = 75
+    val thanksPurchase = "Спасибо за покупку!"
+    println(shopOfOrders)
+    println(thanksPurchase)
+}
