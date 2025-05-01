f6eb4ee3d8cf48aa7b5f50a8a06e40d280597cf6
Alest-58 <emalst@mail.ru>
2025-05-01 21:13:28 +0300

    KS-1-4 create smth

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main () {
+    val length = 40868600000L //(Long)
+    val age = 27               //(Int)
+    val partOfDay = 0.075f   //(Float)
+    val seconds: Short = 6480
+    val partOfYear = 2.0547945205479453E-4 //(Double)
+    val apogee = 327000 //(Int)
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
