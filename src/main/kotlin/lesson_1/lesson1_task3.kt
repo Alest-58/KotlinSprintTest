1efcb51f2d8653a54549dc47a40b6c7e862f8073
Alest-58 <emalst@mail.ru>
2025-05-01 20:02:26 +0300

    KS-1-3 create smth

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main () {
+    val year = 1961
+    val hour = 10
+    val minute = 55
+
+    //println(year)
+    //println(hour)
+    //println(minute)
+
+    println(year)
+    print(hour)
+    print(':')
+    print(minute)
+
+}
