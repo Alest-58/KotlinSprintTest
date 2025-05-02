c0b64f0565bfa269f39d1c6e56f6a4325ba189d3
Alest-58 <emalst@mail.ru>
2025-05-02 18:24:13 +0300

    KS-2-4 task-4

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_2
+
+fun main () {
+
+    val crystallineOre = 7
+    val ironOre = 11
+    val buff = 20
+    val buffCrystallineOre = (crystallineOre * buff)/100
+    val buffironOre = (ironOre * buff)/100
+
+
+    println(buffCrystallineOre)
+    println(buffironOre)
+
+
+}
