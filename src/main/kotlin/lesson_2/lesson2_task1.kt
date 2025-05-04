412d2178dfe9617c683aff85db8c8e2740323d73
Alest-58 <emalst@mail.ru>
2025-05-04 10:49:25 +0300

    KS-2-3 task-3-corrected-last

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_2
 
-fun main () {
-//    println((3+4+3+5)/4f)
+fun main() {
     val firstStudent = 3
     val secondStudent = 4
     val thirdStudent = 3
-    val fourthStudent = 5
-    val averageScore = (firstStudent + secondStudent + thirdStudent + fourthStudent) / 4f
+    val fourthStudent = 5f
 
+    val averageScore = (firstStudent + secondStudent + thirdStudent + fourthStudent) / 4
     println(averageScore)
 }
