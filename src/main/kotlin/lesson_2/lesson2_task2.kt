d0d48375266a9c732637d22bc28f2e8aa9bbf5fd
Alest-58 <emalst@mail.ru>
2025-05-04 11:24:31 +0300

    KS-2-2 task-2-corrected-last

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_2
 
-fun main () {
+fun main() {
     val employees = 50
     val interns = 30
+    val totalNumberOfEmployees = 80
     val salariesOfEmployees = 30000
     val internsSalaries = 20000
+
     val expensesOfPermanentEmployees = employees * salariesOfEmployees
     val totalExpenses = expensesOfPermanentEmployees + interns * internsSalaries
-    val averageCostPerEmployee = totalExpenses / 80
+    val averageCostPerEmployee = totalExpenses / totalNumberOfEmployees
     println(expensesOfPermanentEmployees)
     println(totalExpenses)
     println(averageCostPerEmployee)
