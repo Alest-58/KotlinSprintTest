510ed2211f4c35420cdaa9c6e6452c1f8f9a0e76
Alest-58 <emalst@mail.ru>
2025-05-04 11:53:58 +0300

    KS-2-5 task-5-corrected-last

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,12 +2,14 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-fun main () {
+fun main() {
     val clientAmount = 70000
     val interestRate = 16.7
     val timePeriod = 20
-    val depositAmountAfter20Years = clientAmount. toDouble() * ((1 + interestRate / 100).pow(timePeriod.toDouble()))
+    val one = 1
+    val oneHundred = 100
 
-    println(String.format("%.3f", depositAmountAfter20Years))
-
+    val depositAmountAfter20Years =
+        clientAmount.toDouble() * ((one + interestRate / oneHundred).pow(timePeriod.toDouble()))
+    println("%.3f".format(depositAmountAfter20Years))
 }
