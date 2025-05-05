ad13451748ddafbf78b758eec5e5a843d30d297c
Alest-58 <emalst@mail.ru>
2025-05-05 21:30:56 +0300

    test

---
+++ src/main/kotlin/lesson_1/test.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val surname = "сидорова"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val age = 22
+
+
+    println("$surname $name $patronymic, $age")
+
+}
