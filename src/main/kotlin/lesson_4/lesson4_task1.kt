95a3fa1dfce7a25f15481273b59170729356affa
Alest-58 <emalst@mail.ru>
2025-05-05 14:14:14 +0300

    KS-4-1 task-1

---
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_4
+
+const val TOTAL_TABLES = 13
+
+fun main() {
+    val reservedTablesToday = 13
+    val reservedTablesTomorrow = 9
+    val availabilityOfTablesForToday: Boolean = reservedTablesToday != TOTAL_TABLES
+    val availabilityOfTablesForTomorrow: Boolean = reservedTablesTomorrow < TOTAL_TABLES
+
+    println("Доступность столиков на сегодня:$availabilityOfTablesForToday")
+    println("Доступность столиков на завтра:$availabilityOfTablesForTomorrow")
+}
