cdff932fcdcfa1b46e4ec0e75a5297ae04e38c27
Alest-58 <emalst@mail.ru>
2025-05-05 09:54:53 +0300

    KS-3-2 task-2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val age = 20
+    val surname1 = "Сидорова"
+    val name1 = "Татьяна"
+    val patronymic1 = "Сергеевна"
+    val age1 = 22
+
+    println("$surname $name $patronymic, $age")
+    println("$surname1 $name1 $patronymic1, $age1")
+}
