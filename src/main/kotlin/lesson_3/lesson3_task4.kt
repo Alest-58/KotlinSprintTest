299c961accfe834026c3e3115022718ced67beec
Alest-58 <emalst@mail.ru>
2025-05-06 09:40:16 +0300

    KS-3-4 task-4-corrected

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_3
 
 fun main() {
-    val whereFrom = "E2"
-    val where = "E4"
-    val strokeNumber = 1
-    val whereFrom1 = "D2"
-    val where1 = "D"
-    val strokeNumber1 = 2
+    var whereFrom = "E2"
+    var where = "E4"
+    var strokeNumber = 1
 
     println("$whereFrom - $where ; $strokeNumber")
-    println("$whereFrom1 - $where1${2 + 1} ; $strokeNumber1")
+    whereFrom = "D2"
+    where = "D"
+    strokeNumber = 2
+    println("$whereFrom - $where${2 + 1} ; $strokeNumber")
 }
