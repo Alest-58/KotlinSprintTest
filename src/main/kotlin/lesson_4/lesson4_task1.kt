8d27ae578706b6ecd6e0c6e13f3d0542e6be4801
Alest-58 <emalst@mail.ru>
2025-05-06 15:02:17 +0300

    KS-4-1 task-1-corrected

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -5,8 +5,9 @@ const val TOTAL_TABLES = 13
 fun main() {
     val reservedTablesToday = 13
     val reservedTablesTomorrow = 9
-    val availabilityOfTablesForToday: Boolean = reservedTablesToday != TOTAL_TABLES
+    val availabilityOfTablesForToday = reservedTablesToday < TOTAL_TABLES
     val availabilityOfTablesForTomorrow: Boolean = reservedTablesTomorrow < TOTAL_TABLES
 
-    println("Доступность столиков на сегодня:$availabilityOfTablesForToday\nДоступность столиков на завтра:$availabilityOfTablesForTomorrow")
+    println("Доступность столиков на сегодня:$availabilityOfTablesForToday")
+    println("Доступность столиков на завтра:$availabilityOfTablesForTomorrow")
 }
