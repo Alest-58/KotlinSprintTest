f0215a7220b827efdd3c9018c4e4a267d9bc34d4
Alest-58 <emalst@mail.ru>
2025-05-08 12:50:33 +0300

    KS-5-1 task-1-corrected

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 fun main() {
     val number = Random.nextInt(1, 100)
     val number1 = Random.nextInt(1, 100)
-    println("Сложите два числа $number + $number1")
+    println("Сложите  два числа $number + $number1")
     val numberEntered = readln().toInt()
     val userAnswer = number + number1
 
