45ab692f71a1923a0d2cfa6e8a6fe919351d46a1
Alest-58 <emalst@mail.ru>
2025-05-09 20:06:46 +0300

    KS-4-4 task-4-corrected(2)

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,11 +3,6 @@ package org.example.lesson_4
 fun main() {
     val day = 5
     val isEven = (day % 2) == 0
-    val exercisesForTheHands = !isEven
-    val legExercises = isEven
-    val exercisesForTheBack = isEven
-    val exercisesForThePress = !isEven
-
     println(
         """Упражнения для рук:    ${!isEven}
 Упражнения для ног:    ${isEven}
