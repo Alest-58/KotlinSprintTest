8b6b4b9b78f5daf5c856ab9e9d8d0f553407e1ed
Alest-58 <emalst@mail.ru>
2025-05-09 12:59:26 +0300

    KS-4-3 task-3-corrected

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -3,15 +3,15 @@ package org.example.lesson_4
 const val IS_SUNNY = true
 const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
-const val TIME_OF_THE_YEAR = "зима"
+const val UNFAVORABLE_TIME_OF_YEAR = "зима"
 
 fun main() {
-    val weatherToday = true
-    val awningAtTheMoment = true
+    val isWeatherToday = true
+    val isAwningAtTheMoment = true
     val airHumidity = 20
     val currentTimeOfTheYear = "зима"
-    val result = (IS_SUNNY == weatherToday) && (AIR_HUMIDITY == airHumidity)
-            && (IS_AWNING_OPEN == awningAtTheMoment) && (currentTimeOfTheYear != TIME_OF_THE_YEAR)
+    val result = (IS_SUNNY == isWeatherToday) && (AIR_HUMIDITY == airHumidity) &&
+            (IS_AWNING_OPEN == isAwningAtTheMoment) && (currentTimeOfTheYear != UNFAVORABLE_TIME_OF_YEAR)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 }
