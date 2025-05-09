0ab438571f7647bd68c0c361729fbdd7696bc40a
Alest-58 <emalst@mail.ru>
2025-05-09 12:35:03 +0300

    KS-5-3 task-3-corrected

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,19 +1,22 @@
 package org.example.lesson_5
 
+const val NUMBER_WIN_1 = 10
+const val NUMBER_WIN_2 = 30
+
 fun main() {
     println("Вам нужно угадать два числа! от 0 до 42")
     println("Введите 1 число ")
     val userNumber1 = readln().toInt()
     println("Введите 2 число ")
     val userNumber2 = readln().toInt()
-    val numberWin1 = 10 == userNumber1
-    val numberWin2 = 30 == userNumber2
 
-    if (numberWin1 && numberWin2) {
+    if (NUMBER_WIN_1 == userNumber1 && NUMBER_WIN_2 == userNumber2) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if (numberWin1 || numberWin2) {
+    } else if (NUMBER_WIN_2 == userNumber1 && NUMBER_WIN_1 == userNumber2) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (NUMBER_WIN_1 == userNumber1 || NUMBER_WIN_2 == userNumber2) {
         println("Вы выиграли утешительный приз!")
-    } else if (!numberWin2 || numberWin1) {
+    } else if (NUMBER_WIN_2 == userNumber1 || NUMBER_WIN_1 == userNumber2) {
         println("Вы выиграли утешительный приз!")
     } else println("Неудача!")
     println("Числа для выигрыша 10 и 30!!!")
