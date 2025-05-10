90fd2aa7898a834c0dbd182e95f5719951345f90
Alest-58 <emalst@mail.ru>
2025-05-10 12:39:31 +0300

    KS-5-3 task-3-corrected(2)

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,29 +1,24 @@
 package org.example.lesson_5
 
-const val NUMBER_WIN_1 = 10
-const val NUMBER_WIN_2 = 30
-
 fun main() {
+    val numberWin1 = (0..42).random()
+    val numberWin2 = (0..42).random()
     println("Вам нужно угадать два числа! от 0 до 42")
     println("Введите 1 число ")
     val userNumber1 = readln().toInt()
     println("Введите 2 число ")
     val userNumber2 = readln().toInt()
 
-    if (NUMBER_WIN_1 == userNumber1 && NUMBER_WIN_2 == userNumber2 ||
-        NUMBER_WIN_2 == userNumber1 && NUMBER_WIN_1 == userNumber2
+    if (numberWin1 == userNumber1 && numberWin2 == userNumber2 ||
+        numberWin2 == userNumber1 && numberWin1 == userNumber2
     ) {
         println("Поздравляем! Вы выиграли главный приз!")
-//    } else if (NUMBER_WIN_2 == userNumber1 && NUMBER_WIN_1 == userNumber2) {
-//        println("Поздравляем! Вы выиграли главный приз!")
-    } else if (NUMBER_WIN_1 == userNumber1 || NUMBER_WIN_2 == userNumber2 ||
-        NUMBER_WIN_2 == userNumber1 || NUMBER_WIN_1 == userNumber2
+    } else if (numberWin1 == userNumber1 || numberWin2 == userNumber2 ||
+        numberWin2 == userNumber1 || numberWin1 == userNumber2
     ) {
         println("Вы выиграли утешительный приз!")
-//    } else if (NUMBER_WIN_2 == userNumber1 || NUMBER_WIN_1 == userNumber2) {
-//        println("Вы выиграли утешительный приз!")
     } else println("Неудача!")
-    println("Числа для выигрыша 10 и 30!!!")
+    println("Числа для выигрыша $numberWin1 $numberWin2")
 }
 
 
